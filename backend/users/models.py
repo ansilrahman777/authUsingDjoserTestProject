c00a4c666f0b22924f1b841606bafb2f0d3f1c23
ANSIL RAHMAN <rahmanansil777@gmail.com>
2024-02-22 21:12:40 +0530

    Initial commit

---
+++ backend/users/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from django.utils.translation import gettext_lazy as _
+from .managers import CustomUserManager
+
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    first_name = models.CharField(_("First Name"), max_length=50)
+    last_name = models.CharField(_("Last Name"), max_length=50)
+    email = models.EmailField(_("Email Address"), max_length=254, unique = True)
+    mobile = models.CharField(_("Mobile"), max_length=15, unique=True)
+
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+    is_blocked = models.BooleanField(default=False)
+    date_joined = models.DateTimeField(auto_now_add=True)
+
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["first_name","last_name"]
+
+
+    objects = CustomUserManager()
+
+    class Meta:
+        verbose_name = _("User")
+        verbose_name_plural = _("Users")
+
+    def __str__(self):
+        return self.email
+
+    @property
+    def get_full_name(self):
+        return f"{self.first_name} {self.last_name}"
